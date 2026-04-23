b5b6d603b0b7dac9da072a8a8ca865e6645a3f81
sagitta42 <mariia.redchuk@gmail.com>
2026-04-23 22:53:45 +0200

    display package tree

--- src/poetic/core.py
+++ src/poetic/core.py
@@ -1,14 +1,20 @@
 import os
 from pathlib import Path
 
+from poetic.tree import tree
+
 
 def setup_package_template(package_name: str):
     print(f"Setting up package: {package_name}")
     os.system(f"poetry new {package_name}")
 
-    path_to_src: Path = Path(package_name) / "src" / package_name
+    package_path: Path = Path(package_name)
+    path_to_src: Path = package_path / "src" / package_name
     f = open(path_to_src / "core.py", "w")
     f.close()
 
     with open(path_to_src / "__init__.py", "a") as f:
         f.write("from core import *")
+
+    for line in tree(package_path):
+        print(line)
---
+++ src/poetic/tree.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+space = "    "
+branch = "│   "
+tee = "├── "
+final = "└── "
+
+
+def tree(path: Path, prefix: str = ""):
+    """
+    Display directory tree akin to bash tree.
+
+    A recursive generator, given a directory Path object
+        will yield a visual tree structure akin to that of bash/GNU tree
+
+    https://stackoverflow.com/questions/9727673/list-directory-tree-structure-in-python
+    """
+    contents = list(path.iterdir())
+    pointers = [tee] * (len(contents) - 1) + [final]
+    for pointer, path in zip(pointers, contents):
+        yield prefix + pointer + path.name
+        if path.is_dir():
+            extension = branch if pointer == tee else space
+            yield from tree(path, prefix=prefix + extension)
