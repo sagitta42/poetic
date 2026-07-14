b2b8ef48d1ff0266d39f97ac7a9a3dbdb224f7de
sagitta42 <mariia.redchuk@gmail.com>
2026-07-14 12:25:34 +0200

    git commands management

---
+++ src/poetic/git.py
@@ -0,0 +1,106 @@
+from pathlib import Path
+import subprocess
+from typing import Callable
+
+
+class Git:
+    """
+    Git operations management.
+
+    Simple management utilizing subprocess.
+    """
+
+    def __init__(self, path: Path | None = None) -> None:
+        self.path = path or Path.cwd()
+
+    def run(self, *args, check: bool = False) -> str | None:
+        """
+        Simple command run in template root directory.
+
+        check (bool): check and return command output
+        """
+        action = self._get_command_output if check else self._run_command
+        ret = action(args)
+        return ret
+
+    def get_active_branch(self) -> str:
+        """
+        Get name of active branch.
+        """
+        check = self._get_command_output("rev-parse", "--abbrev-ref", "HEAD")
+        ret = check.strip()
+        return ret
+
+    def branch_exists(self, branch_name: str) -> bool:
+        branches = self.get_branch_list()
+        ret = branch_name in branches
+        return ret
+
+    def commit_all(self, commit_message: str):
+        """
+        Add and commit all files.
+        """
+        self._run_command("add", "*")
+        self._run_command("commit", "-am", commit_message)
+
+    def get_last_commit(self) -> str:
+        """
+        Get hash of last commit.
+        """
+        output = self._get_command_list_output("rev-list", "HEAD")
+        ret = output[0]
+        return ret
+
+    def get_first_commit(self) -> str:
+        """
+        Get hash of first commit.
+        """
+        output = self._get_command_list_output("rev-list", "HEAD")
+        ret = output[-1]
+        return ret
+
+    def get_branch_list(self) -> list[str]:
+        """
+        Get list of branch names
+        """
+        output = self._get_command_list_output("branch", "--list")
+
+        def clean_branch_name(name: str) -> str:
+            ret = name
+            if ret.startswith("*"):
+                ret = ret[1:]
+            ret = ret.strip()
+            return ret
+
+        ret = [clean_branch_name(branch) for branch in output]
+        return ret
+
+    def get_commit_message(self, commit: str) -> str:
+        """
+        Get commit message of given commit hash.
+        """
+        output = self._get_command_list_output("show", "--quiet", commit)
+        ret = output[-1]
+        return ret
+
+    def _get_command_list_output(self, *args) -> list[str]:
+        """
+        Get command output in list form.
+
+        Convert single string output into list.
+        """
+        output = self._get_command_output(*args)
+        split_output = output.split("\n")
+        ret = [out.strip() for out in split_output if not out == ""]
+        return ret
+
+    def _run_command(self, *args) -> None:
+        self._run_subprocess(subprocess.run, *args)
+
+    def _get_command_output(self, *args) -> str:
+        ret = self._run_subprocess(subprocess.check_output, *args, text=True)
+        return ret
+
+    def _run_subprocess(self, subprocess_func: Callable, *args, **kwargs) -> str | None:
+        ret = subprocess_func(["git"] + list(args), cwd=self.path, **kwargs)
+        return ret
---
+++ tests/test_git.py
@@ -0,0 +1,34 @@
+from poetic.git import Git
+
+from poetic.logger import logg
+
+
+def test_active_branch():
+    git = Git()
+
+    active_branch = git.get_active_branch()
+    logg.info(f"Active branch: {active_branch}")
+
+
+def test_branch_exists():
+    git = Git()
+
+    check = git.branch_exists("main")
+    assert check
+
+
+def test_first_commit():
+    git = Git()
+
+    first_commit = git.get_first_commit()
+
+    assert first_commit == "44aeefafcb3af5c8591ae70cff11147afd1268f2"
+
+
+def test_last_commit_message():
+    git = Git()
+
+    last_commit = git.get_last_commit()
+    last_commit_message = git.get_commit_message(last_commit)
+
+    logg.info(f"Last commit message: {last_commit_message}")
