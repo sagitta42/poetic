cc5e7f2b08a96ff616754dc64b83b48034feb6a9
sagitta42 <mariia.redchuk@gmail.com>
2026-08-05 10:22:03 +0200

    logger update: different formats for logging levels, determine correct stack level

--- src/poetic/logger.py
+++ src/poetic/logger.py
@@ -1,4 +1,5 @@
 import enum
+import inspect
 import logging
 from dotenv import dotenv_values
 
@@ -39,28 +40,90 @@ class AnsiColor(str, enum.Enum):
         return self.value
 
 
+class LevelFormatter(logging.Formatter):
+    def __init__(self, formats, default_fmt=None, datefmt=None):
+        super().__init__(datefmt=datefmt)
+        self.formats = {
+            level: logging.Formatter(fmt, datefmt=datefmt)
+            for level, fmt in formats.items()
+        }
+        self.default_formatter = logging.Formatter(
+            default_fmt or "%(message)s", datefmt=datefmt
+        )
+
+    def format(self, record):
+        formatter = self.formats.get(record.levelno, self.default_formatter)
+        return formatter.format(record)
+
+
 class Logger:
     def __init__(self, log_level=logging.INFO):
         self._logger = logging.getLogger(__name__)
-        self._logger.setLevel(log_level)
+
+        full_format = "%(asctime)s [%(levelname)s] %(classname)s.%(funcName)s:%(lineno)d - %(message)s"
+        short_format = "[%(levelname)s] %(classname)s - %(message)s"
+        formatter = LevelFormatter(
+            {
+                logging.DEBUG: full_format,
+                logging.ERROR: full_format,
+                logging.INFO: short_format,
+                logging.WARNING: short_format,
+            }
+        )
+
         handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
         handler.setLevel(log_level)
         self._logger.addHandler(handler)
 
+        self._logger.setLevel(log_level)
+        self._logger.propagate = False
+
     def info(self, message: str, header: bool = False):
         color = AnsiColor.green if header else AnsiColor.white
-        return self._logger.info(color.apply(message, header))
+        return self._log(logging.INFO, color.apply(message, header))
 
     def error(self, message: str):
-        return self._logger.error(AnsiColor.red.apply(message))
+        return self._log(logging.ERROR, AnsiColor.red.apply(message))
 
     def warning(self, message: str, important: bool = False):
         if important:
             message = f"! WARNING ! {message}"
-        return self._logger.warning(AnsiColor.yellow.apply(message, important))
+        return self._log(logging.WARNING, AnsiColor.yellow.apply(message, important))
 
     def debug(self, message: str):
-        return self._logger.debug(AnsiColor.grey.apply(message))
+        return self._log(logging.DEBUG, AnsiColor.grey.apply(message))
+
+    def _log(self, level, message: str):
+        """
+        Common log interface for info/error/warning/debug.
+
+        Auto-detect class name of caller.
+        Account for stack level to display correct funcName and lineno.
+            Skip 2 stack levels including this method,
+            the info/error/warning/debug method calling it,
+        """
+        self._logger.log(
+            level,
+            message,
+            extra={"classname": self._get_caller_class_name()},
+            stacklevel=3,
+        )
+
+    def _get_caller_class_name(self):
+        """
+        Determine class name of where logger is called from.
+
+        Obtain frame index 3 corresponding to actual caller
+            (0 = this method, 1 = _log internal method, 2 = info/warning/error/debug logger call).
+        Return class or module name of that frame.
+        """
+        frame = inspect.stack()[3].frame
+        if "self" in frame.f_locals:
+            return type(frame.f_locals["self"]).__name__
+        elif "cls" in frame.f_locals:
+            return frame.f_locals["cls"].__name__
+        return frame.f_globals.get("__name__", "-")
 
 
 env_config = dotenv_values()
